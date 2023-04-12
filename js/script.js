a0f8a8baddcccd8398e2df6909e97d7732f4d362
EverettBernard <124380095+EverettBernard@users.noreply.github.com>
2023-04-12 14:43:28 +0000

    Almost finished

--- js/script.js
+++ js/script.js
@@ -7,30 +7,29 @@
  * This function calculates Volume of a cone.
  */
 function calculate () {
-  // input
+  // user inputs radius and height
   let ValueR = parseFloat(document.getElementById('radius').value);
   let ValueH = parseFloat(document.getElementById('height').value);
   
-  // process
+  // calculates volume
   let VolumeCone = 1/3 * Math.PI *ValueR**2 *ValueH
   
-  // output
-  document.getElementById('volumeC-calculate').innerHTML = 'The volume of the cone is is: ' + VolumeCone.toFixed(2) + " cm3"
+  // displays volume
+  document.getElementById('volumeC-calculate').innerHTML = 'The volume of the cone is: ' + VolumeCone.toFixed(2) + " cm3"
   
-
 }
 /**
  * This function calculates Volume of a Pyramid
  */
-function calculate () {
-  // input
+function calculatevol () {
+  // user inputs length, width, height
   let ValueL = parseFloat(document.getElementById('length').value);
   let ValueW = parseFloat(document.getElementById('width').value);
-  let ValueH = parseFloat(document.getElementById('height2').value);
+  let ValueH2 = parseFloat(document.getElementById('height2').value);
 
-  // process
-  let VolumeCone = 1/3 * Math.PI *ValueR**2 *ValueH
+  // calculates volume
+  let VolumePyr = (ValueH2 * ValueL * ValueW) /3
   
-  // output
-  document.getElementById('volumeC-calculate').innerHTML = 'The volume of the cone is is: ' + VolumeCone.toFixed(2) + " cm3"
-  
+  // displays volume
+  document.getElementById('volumeV-calculate').innerHTML = 'The volume of the pyramid is: ' + VolumePyr.toFixed(2) + " cm3"
+}
